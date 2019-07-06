632c64c7a3ca95da26b9976db8a699faab5da534
huybui <huythdnbkltv@gmail.com>
2019-07-06 12:09:48 +0300

    Set up DataBinding for QuizAskFragment

--- app/src/main/java/com/example/quiz/ui/QuizAskFragment.kt
+++ app/src/main/java/com/example/quiz/ui/QuizAskFragment.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.quiz.R
-import kotlinx.android.synthetic.main.fragment_container.*
+import com.example.quiz.databinding.FragmentQuizAskBinding
 
 class QuizAskFragment : Fragment() {
+    private lateinit var binding: FragmentQuizAskBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view : View = inflater.inflate(R.layout.fragment_container, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_container, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-
-        return view;
     }
 }
