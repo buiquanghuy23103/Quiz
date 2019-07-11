ec7dabc9d022b8a268b15dff93d7a3c074e651ae
huybui <huythdnbkltv@gmail.com>
2019-07-11 18:00:43 +0300

    Create DataRepository

---
+++ app/src/main/java/com/example/quiz/database/DataRepository.kt
@@ -0,0 +1,14 @@
+package com.example.quiz.database
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.example.quiz.model.Quiz
+
+class DataRepository(val application: Application) {
+    private val database = AppDatabase.getInstance(application.applicationContext)
+    private val quizDao = database.quizDao
+
+    fun getAllQuizzes(): LiveData<List<Quiz>>{
+        return quizDao.getAllQuizzes()
+    }
+}
