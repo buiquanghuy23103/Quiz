74d0e74cc1fa1bf46de1102b19798c2fbc47f322
huybui <huythdnbkltv@gmail.com>
2019-07-11 15:23:48 +0300

    Set all variables to constant (DbScheme)

--- app/src/main/java/com/example/quiz/database/DbScheme.kt
+++ app/src/main/java/com/example/quiz/database/DbScheme.kt
@@ -2,19 +2,19 @@ package com.example.quiz.database
 
 class DbScheme {
     companion object{
-        val DATABASE_NAME = "quizBase.db"
+        const val DATABASE_NAME = "quizBase.db"
     }
 
     class QuizTable{
         companion object{
-            val TABLE_NAME = "quizTable"
+            const val TABLE_NAME = "quizTable"
         }
 
         class Cols{
             companion object{
-                val ID = "id"
-                val QUESTION = "question"
-                val ANSWER = "answer"
+                const val ID = "id"
+                const val QUESTION = "question"
+                const val ANSWER = "answer"
             }
         }
     }
--- app/src/main/java/com/example/quiz/model/Quiz.kt
+++ app/src/main/java/com/example/quiz/model/Quiz.kt
@@ -1,8 +1,10 @@
 package com.example.quiz.model
 
+import androidx.room.Entity
+import com.example.quiz.database.DbScheme
 import kotlin.random.Random
 
-
+@Entity(tableName = DbScheme.QuizTable.TABLE_NAME)
 data class Quiz(
     var question : Int,
     var answer : Boolean
