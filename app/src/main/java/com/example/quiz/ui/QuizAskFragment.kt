18ee87741102d79343e49f4f26105710ebaad2c3
huybui <huythdnbkltv@gmail.com>
2019-07-12 15:43:52 +0300

    Update QuizAskViewModel and QuizAskFragment to use LiveData<Quiz>

--- app/src/main/java/com/example/quiz/ui/QuizAskFragment.kt
+++ app/src/main/java/com/example/quiz/ui/QuizAskFragment.kt
@@ -1,19 +1,20 @@
 package com.example.quiz.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
 import com.example.quiz.R
 import com.example.quiz.databinding.QuizAskFragmentBinding
 import com.example.quiz.model.Quiz
 import com.example.quiz.viewmodel.QuizAskViewModel
+import com.example.quiz.viewmodel.QuizAskViewModelFactory
 import kotlin.math.abs
 
 class QuizAskFragment : Fragment() {
@@ -29,35 +30,38 @@ class QuizAskFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.quiz_ask_fragment, container, false)
-        viewModel = ViewModelProviders.of(this).get(QuizAskViewModel::class.java)
-        quizBank = viewModel.quizBank
-        for (i in 0..(quizBank.size-1)){
-            if (quizBank[i].id == args.quizId){
-                viewModel.currentIndex = i
-                Log.i(TAG, "index = " + i)
-                break
+
+        val app = requireNotNull(this.activity).application
+        val factory = QuizAskViewModelFactory(app, args.quizId)
+        viewModel = ViewModelProviders.of(this, factory).get(QuizAskViewModel::class.java)
+
+        viewModel.quiz.observe(this, Observer { quiz ->
+            quiz?.let {
+                updateUI(quiz)
             }
-        }
-        updateQuestion()
+        })
+
         return binding.root
+    }
+
+    private fun updateUI(quiz: Quiz){
+        binding.quiz = quiz
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.trueButton.setOnClickListener { checkAnswer(true) }
         binding.falseButton.setOnClickListener { checkAnswer(false) }
         binding.nextButton.setOnClickListener{
-            viewModel.currentIndex = (viewModel.currentIndex + 1) % quizBank.size
-            updateQuestion()
+            viewModel.moveForward()
         }
 
         binding.backButton.setOnClickListener {
-            viewModel.currentIndex = abs(viewModel.currentIndex - 1) % quizBank.size
-            updateQuestion()
+            viewModel.moveBack()
         }
     }
 
     private fun checkAnswer(userPressed: Boolean){
-        if (userPressed == quizBank[viewModel.currentIndex].answer){
+        if (userPressed == true){
             toast(R.string.correct_toast)
         } else {
             toast(R.string.incorrect_toast)
@@ -66,9 +70,5 @@ class QuizAskFragment : Fragment() {
 
     private fun toast(messageId : Int){
         Toast.makeText(activity, messageId, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun updateQuestion(){
-        binding.questTextView.text = quizBank[viewModel.currentIndex].question
     }
 }
--- app/src/main/java/com/example/quiz/viewmodel/QuizAskViewModel.kt
+++ app/src/main/java/com/example/quiz/viewmodel/QuizAskViewModel.kt
@@ -1,15 +1,46 @@
 package com.example.quiz.viewmodel
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-import com.example.quiz.R
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.quiz.database.DataGenerator
 import com.example.quiz.model.Quiz
+import kotlin.math.abs
 
 class QuizAskViewModel(private val app: Application, private val id: Int) : AndroidViewModel(app) {
-    val quizBank = DataGenerator.quizzes
+    private val TAG = "QuizAskViewModel"
 
-    var currentIndex : Int = 0
+    private val quizBank = DataGenerator.quizzes
+    private var currentIndex : Int = 0
 
+    private var _quiz = MutableLiveData<Quiz>()
+    val quiz: LiveData<Quiz>
+        get() = _quiz
+
+    init {
+        for (i in 0..(quizBank.size-1)){
+            if (quizBank[i].id == id){
+                currentIndex = i
+                updateQuiz()
+                Log.i(TAG, "currentIndex = " + i)
+                break
+            }
+        }
+    }
+
+    private fun updateQuiz(){
+        _quiz.value = quizBank[currentIndex]
+    }
+
+    fun moveBack(){
+        currentIndex = abs(currentIndex - 1) % quizBank.size
+        updateQuiz()
+    }
+
+    fun moveForward(){
+        currentIndex = (currentIndex + 1) % quizBank.size
+        updateQuiz()
+    }
 }
