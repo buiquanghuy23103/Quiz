b76b0ef8b3e4e88a66282d2b03233326bcec41ab
huybui <huythdnbkltv@gmail.com>
2019-07-18 06:59:36 +0300

    Add navigations towards QuizEditFragment

--- app/src/main/java/com/example/quiz/ui/BaseActivity.kt
+++ app/src/main/java/com/example/quiz/ui/BaseActivity.kt
@@ -10,7 +10,7 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_container)
+        setContentView(R.layout.activity_main)
 
         var fragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
         if (fragment == null){
