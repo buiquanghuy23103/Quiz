8af14b4e118fcfed6779ed4bae34b5f6f90990c3
huybui <huythdnbkltv@gmail.com>
2019-07-18 09:34:43 +0300

    Add navigation to QuizEditFragment in QuizListFragment

--- app/src/main/java/com/example/quiz/ui/QuizListFragment.kt
+++ app/src/main/java/com/example/quiz/ui/QuizListFragment.kt
@@ -3,12 +3,11 @@ package com.example.quiz.ui
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 
 import com.example.quiz.R
@@ -39,6 +38,25 @@ class QuizListFragment : Fragment() {
             }
         })
 
+        setHasOptionsMenu(true)
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.quiz_list_fragment, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId){
+            R.id.menu_item_quiz_list_add -> startQuizEditFragment(item)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun startQuizEditFragment(item: MenuItem?){
+        val newQuiz = Quiz()
+        val action = QuizListFragmentDirections.actionQuizListFragmentToQuizEditFragment(newQuiz.id)
+        item!!.actionView.findNavController().navigate(action)
     }
 }
