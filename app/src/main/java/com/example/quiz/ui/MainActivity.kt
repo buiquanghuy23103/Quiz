83f788b3c31cc02ad15e4ae88164d3fb65b816d8
huybui <huythdnbkltv@gmail.com>
2019-07-18 18:10:04 +0300

    Delete BaseActivity

--- app/src/main/java/com/example/quiz/ui/MainActivity.kt
+++ app/src/main/java/com/example/quiz/ui/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.quiz.ui
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.quiz.R
 
-class MainActivity : BaseActivity() {
-    override fun fragment(): Fragment {
-        return QuizListFragment.newInstance()
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
