79a967b04dab8dc385659e87b5c668f731e8f9e4
huybui <huythdnbkltv@gmail.com>
2019-07-18 19:12:54 +0300

    Add "up" button in app bar

--- app/src/main/java/com/example/quiz/ui/MainActivity.kt
+++ app/src/main/java/com/example/quiz/ui/MainActivity.kt
@@ -3,11 +3,23 @@ package com.example.quiz.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.quiz.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Add an up button in app bar
+        navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
--- app/src/main/java/com/example/quiz/ui/QuizEditFragment.kt
+++ app/src/main/java/com/example/quiz/ui/QuizEditFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 
 import com.example.quiz.R
