0ba630be8eb6693539605604fc2b87a4640c0e1c
huybui <huythdnbkltv@gmail.com>
2019-07-18 10:46:28 +0300

    Add QuizEditViewModelFactory

--- app/src/main/java/com/example/quiz/viewmodel/QuizEditViewModel.kt
+++ app/src/main/java/com/example/quiz/viewmodel/QuizEditViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.quiz.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel;
 
-class QuizEditViewModel : ViewModel() {
+class QuizEditViewModel(app: Application, private val id: Int) : AndroidViewModel(app) {
     // TODO: Implement the ViewModel
 }
---
+++ app/src/main/java/com/example/quiz/viewmodel/QuizEditViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.quiz.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.lang.IllegalArgumentException
+
+class QuizEditViewModelFactory(private val app: Application, private val index: Int): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(QuizEditViewModel::class.java)){
+            return QuizEditViewModel(app, index) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
