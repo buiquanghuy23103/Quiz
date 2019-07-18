a57d53bff8641c3044837ae77331699d2dac6064
huybui <huythdnbkltv@gmail.com>
2019-07-18 11:16:18 +0300

    Add databinding and initialize viewmodel in QuizEditFragment

--- app/src/main/java/com/example/quiz/ui/QuizEditFragment.kt
+++ app/src/main/java/com/example/quiz/ui/QuizEditFragment.kt
@@ -6,29 +6,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.navArgs
 
 import com.example.quiz.R
+import com.example.quiz.databinding.QuizEditFragmentBinding
+import com.example.quiz.model.Quiz
 import com.example.quiz.viewmodel.QuizEditViewModel
+import com.example.quiz.viewmodel.QuizEditViewModelFactory
+import kotlinx.android.synthetic.main.quiz_ask_fragment.*
 
 class QuizEditFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = QuizEditFragment()
-    }
-
     private lateinit var viewModel: QuizEditViewModel
+    private lateinit var binding: QuizEditFragmentBinding
+    private val args: QuizEditFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.quiz_edit_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.quiz_edit_fragment, container, false)
+
+        val app = requireNotNull(this.activity).application
+        val factory = QuizEditViewModelFactory(app, args.quizId)
+        viewModel = ViewModelProviders.of(this, factory).get(QuizEditViewModel::class.java)
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(QuizEditViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.quiz = Quiz("New question")
     }
-
 }
--- app/src/main/java/com/example/quiz/viewmodel/QuizEditViewModelFactory.kt
+++ app/src/main/java/com/example/quiz/viewmodel/QuizEditViewModelFactory.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider
 import java.lang.IllegalArgumentException
 
 class QuizEditViewModelFactory(private val app: Application, private val index: Int): ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(QuizEditViewModel::class.java)){
             return QuizEditViewModel(app, index) as T
