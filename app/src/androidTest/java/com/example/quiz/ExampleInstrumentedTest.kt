d16ca6075fbdaeba9e2e3e0cf6e0fa7ee8f8d4cb
huybui <huythdnbkltv@gmail.com>
2019-07-22 17:44:36 +0300

    Add Espresso dependencies

--- app/src/androidTest/java/com/example/quiz/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/quiz/ExampleInstrumentedTest.kt
@@ -1,12 +1,13 @@
 package com.example.quiz
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
-
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +16,6 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.quiz", appContext.packageName)
-    }
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
 }
