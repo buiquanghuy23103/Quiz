a5525b2902e0cbd08ddaa6028ac6129151694f4d
huybui <huythdnbkltv@gmail.com>
2019-07-22 10:24:59 +0300

    Set RepositoryComponent as Singleton

--- app/src/main/java/com/example/quiz/dagger/RepositoryComponent.kt
+++ app/src/main/java/com/example/quiz/dagger/RepositoryComponent.kt
@@ -2,7 +2,9 @@ package com.example.quiz.dagger
 
 import com.example.quiz.database.DataRepository
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [AppModule::class])
 interface RepositoryComponent {
     fun getRepository(): DataRepository
