319032b312847526012f877072070c8826d27303
huybui <huythdnbkltv@gmail.com>
2019-07-22 11:14:39 +0300

    Add ForeignKey and Indices to Answer model

--- app/src/main/java/com/example/quiz/model/Answer.kt
+++ app/src/main/java/com/example/quiz/model/Answer.kt
@@ -2,10 +2,21 @@ package com.example.quiz.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import com.example.quiz.database.DbScheme.AnswerTable
+import com.example.quiz.database.DbScheme.QuizTable
 import kotlin.random.Random
 
-@Entity(tableName = AnswerTable.TABLE_NAME)
+@Entity(tableName = AnswerTable.TABLE_NAME,
+        foreignKeys = [ForeignKey(
+            entity = Quiz::class,
+            parentColumns = [QuizTable.Cols.ID],
+            childColumns = [AnswerTable.Cols.QUIZ_ID],
+            onDelete = ForeignKey.CASCADE
+        )],
+        indices = [Index(value = [AnswerTable.Cols.QUIZ_ID])]
+    )
 data class Answer(
     @ColumnInfo(name = AnswerTable.Cols.ID)
     val id : Int = Random.nextInt(),
