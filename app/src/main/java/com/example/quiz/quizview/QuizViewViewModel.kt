8ae404afe5b497c5b77094edb38fae158c019332
huybui <huythdnbkltv@gmail.com>
2019-07-23 12:04:58 +0300

    Refactor QuizViewViewModel

--- app/src/main/java/com/example/quiz/quizview/QuizViewViewModel.kt
+++ app/src/main/java/com/example/quiz/quizview/QuizViewViewModel.kt
@@ -5,12 +5,7 @@ import com.example.quiz.BaseViewModel
 import com.example.quiz.model.Answer
 import com.example.quiz.model.Quiz
 
-class QuizViewViewModel(private var id: Int) : BaseViewModel() {
-    val quiz: LiveData<Quiz>
-    val answerList: LiveData<List<Answer>>
-
-    init {
-        quiz = repository.getQuizById(id)
-        answerList = repository.getAnswersByQuizId(id)
-    }
+class QuizViewViewModel(id: Int) : BaseViewModel() {
+    val quiz = repository.getQuizById(id)
+    val answerList = repository.getAnswersByQuizId(id)
 }
