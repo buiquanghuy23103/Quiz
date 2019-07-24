80346c8204ac93a4b8f9e274e250ff30c36cecdb
huybui <huythdnbkltv@gmail.com>
2019-07-24 17:39:14 +0300

    Move AnswerEditItem class to inside AnswerEditAdapter

--- app/src/main/java/com/example/quiz/quizedit/AnswerEditAdapter.kt
+++ app/src/main/java/com/example/quiz/quizedit/AnswerEditAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.quiz.quizedit
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.quiz.databinding.AnswerEditItemBinding
 import com.example.quiz.model.Answer
 import com.example.quiz.quizview.AnswerViewItem
 
-class AnswerEditAdapter(private val answerList: List<Answer>) : RecyclerView.Adapter<AnswerEditItem>(){
+class AnswerEditAdapter(private val answerList: List<Answer>) : RecyclerView.Adapter<AnswerEditAdapter.AnswerEditItem>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnswerEditItem {
         return AnswerEditItem.from(parent)
     }
@@ -14,5 +16,21 @@ class AnswerEditAdapter(private val answerList: List<Answer>) : RecyclerView.Ada
 
     override fun onBindViewHolder(holder: AnswerEditItem, position: Int) {
         holder.bind(answerList[position])
+    }
+
+    class AnswerEditItem private constructor(private val binding: AnswerEditItemBinding)
+        : RecyclerView.ViewHolder(binding.root){
+
+        companion object{
+            fun from(parent: ViewGroup): AnswerEditItem{
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = AnswerEditItemBinding.inflate(inflater)
+                return AnswerEditItem(binding)
+            }
+        }
+
+        fun bind(answer: Answer){
+            binding.answer = answer
+        }
     }
 }
