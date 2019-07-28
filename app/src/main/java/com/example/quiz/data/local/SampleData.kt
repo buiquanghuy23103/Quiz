255d3b648611d0f62adef7332e1c284799a09c9f
huybui <huythdnbkltv@gmail.com>
2019-07-28 10:06:19 +0300

    Update Gradle plugin; Make SampleData singleton

--- app/src/main/java/com/example/quiz/data/local/SampleData.kt
+++ app/src/main/java/com/example/quiz/data/local/SampleData.kt
@@ -4,29 +4,27 @@ import com.example.quiz.model.Answer
 import com.example.quiz.model.Quiz
 import java.util.*
 
-class SampleData {
-    companion object{
-        val sampleQuizList = listOf(
-            Quiz("Canberra is the capital of Australia.", false),
-            Quiz("The Pacific Ocean is larger than the Atlantic Ocean.", true),
-            Quiz("The Suez Canal connects the Red Sea and the Indian Ocean.", true),
-            Quiz("The source of the Nile River is in Egypt.", false),
-            Quiz("The Amazon River is the longest river in the Americas.", true),
-            Quiz("Lake Baikal is the world\'s oldest and deepest freshwater lake.", true)
-        )
+object SampleData {
+    val sampleQuizList = listOf(
+        Quiz("Canberra is the capital of Australia.", false),
+        Quiz("The Pacific Ocean is larger than the Atlantic Ocean.", true),
+        Quiz("The Suez Canal connects the Red Sea and the Indian Ocean.", true),
+        Quiz("The source of the Nile River is in Egypt.", false),
+        Quiz("The Amazon River is the longest river in the Americas.", true),
+        Quiz("Lake Baikal is the world\'s oldest and deepest freshwater lake.", true)
+    )
 
-        val sampleAnswerList = generateAnswerList()
+    val sampleAnswerList = generateAnswerList()
 
-        private fun generateAnswerList(): List<Answer>{
-            var list = ArrayList<Answer>()
-            sampleQuizList.forEach {
-                for (i in 1..4){
-                    val answer = Answer(it.id, "Answer " + i, i%2 == 0)
-                    list.add(answer)
-                }
+    private fun generateAnswerList(): List<Answer> {
+        var list = ArrayList<Answer>()
+        sampleQuizList.forEach {
+            for (i in 1..4) {
+                val answer = Answer(it.id, "Answer " + i, i % 2 == 0)
+                list.add(answer)
             }
-
-            return list
         }
+
+        return list
     }
 }
