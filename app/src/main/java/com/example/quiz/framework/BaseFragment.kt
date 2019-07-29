3c29c345151fedd9a73322d704a1a4ad058c3e04
huybui <huythdnbkltv@gmail.com>
2019-07-29 13:37:06 +0300

    Create inline function getViewModel() as a shortcut to create ViewModel

--- app/src/main/java/com/example/quiz/framework/BaseFragment.kt
+++ app/src/main/java/com/example/quiz/framework/BaseFragment.kt
@@ -1,10 +1,18 @@
 package com.example.quiz.framework
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 
 abstract class BaseFragment<T: BaseViewModel>: Fragment() {
     abstract fun initViewModel(): T
     protected val viewModel by lazy {
         initViewModel()
+    }
+
+    inline fun <reified T : BaseViewModel> Fragment.getViewModel(noinline creator: (() -> T)? = null): T {
+        return if (creator == null)
+            ViewModelProviders.of(this).get(T::class.java)
+        else
+            ViewModelProviders.of(this, BaseViewModelFactory(creator)).get(T::class.java)
     }
 }
