e2faa5567b77ab9bb1b4b0b3449bcb4657635ff5
huybui <huythdnbkltv@gmail.com>
2019-07-31 09:53:30 +0300

    Create a test to check the visibility of answer_view_recycler_view

---
+++ app/src/androidTest/java/com/example/quiz/TestUtils.kt
@@ -0,0 +1,14 @@
+package com.example.quiz
+
+import com.example.quiz.model.Quiz
+
+val sampleQuizList = listOf(
+    Quiz("Canberra is the capital of Australia.", 0),
+    Quiz("The Pacific Ocean is larger than the Atlantic Ocean.", 1),
+    Quiz("The Suez Canal connects the Red Sea and the Indian Ocean.", 2),
+    Quiz("The source of the Nile River is in Egypt.", 3),
+    Quiz("The Amazon River is the longest river in the Americas.", 4),
+    Quiz("Lake Baikal is the world\'s oldest and deepest freshwater lake.", 5)
+)
+
+val sampleQuiz = sampleQuizList[0]
---
+++ app/src/androidTest/java/com/example/quiz/quizview/QuizViewFragmentTest.kt
@@ -0,0 +1,43 @@
+package com.example.quiz.quizview
+
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.example.quiz.MainActivity
+import com.example.quiz.R
+import org.hamcrest.CoreMatchers.allOf
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class QuizViewFragmentTest {
+    @Rule
+    @JvmField
+    val activityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    fun jumpToQuizViewFragment() {
+        activityTestRule.activity.apply {
+            runOnUiThread {
+                val bundle = Bundle().apply { putInt("index", 2) }
+                findNavController(R.id.nav_host_fragment).navigate(
+                    R.id.quizAskPagerFragment,
+                    bundle
+                )
+            }
+        }
+    }
+
+    @Test
+    fun testAnswerListIsDisplayed() {
+        onView(allOf(withId(R.id.answer_view_recycler_view)))
+            .check(matches(isDisplayed()))
+    }
+}
