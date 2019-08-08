4be52cc720358aac23ca9fd423c43504835c2e5a
huybui <huythdnbkltv@gmail.com>
2019-08-08 09:18:14 +0300

    Create an instance of AppComponent in MainApplication

--- app/src/main/java/com/example/quiz/MainApplication.kt
+++ app/src/main/java/com/example/quiz/MainApplication.kt
@@ -1,15 +1,25 @@
 package com.example.quiz
 
 import android.app.Application
+import com.example.quiz.dagger.AppComponent
+import com.example.quiz.dagger.AppModule
+import com.example.quiz.dagger.DaggerAppComponent
 import timber.log.Timber
 
 class MainApplication: Application() {
+    lateinit var component: AppComponent
     companion object{
-        lateinit var INSTANCE: MainApplication
+        private var INSTANCE: MainApplication? = null
+
+        @JvmStatic
+        fun get(): MainApplication = INSTANCE!!
     }
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         INSTANCE = this
+        component = DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
     }
 }
