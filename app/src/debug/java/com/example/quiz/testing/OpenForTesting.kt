51069022908fccd5d3bbec404314131b9ae90206
huybui <huythdnbkltv@gmail.com>
2019-08-09 15:00:27 +0300

    Replace Mock-maker with kotlin-allopen

---
+++ app/src/debug/java/com/example/quiz/testing/OpenForTesting.kt
@@ -0,0 +1,18 @@
+package com.example.quiz.testing
+
+// Forked from https://github.com/googlesamples/android-architecture-components/blob/d81da2cb1e3d61e40f052e631bb15883d0f9f637/GithubBrowserSample/app/src/debug/java/com/android/example/github/testing/OpenForTesting.kt
+
+
+/**
+ * This annotation allows us to open some classes for mocking purposes while they are final in
+ * release builds.
+ */
+@Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS)
+annotation class OpenClass
+
+/**
+ * Annotate a class with [OpenForTesting] if you want it to be extendable in debug builds.
+ */
+@OpenClass
+@Target(AnnotationTarget.CLASS)
+annotation class OpenForTesting
