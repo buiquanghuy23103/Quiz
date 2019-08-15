3ac86698ea0770aa5cbcdd88e9df282e877e6e15
huybui <huythdnbkltv@gmail.com>
2019-08-15 17:00:15 +0300

    Add Room annotations to Category.kt

--- app/src/main/java/com/example/quiz/model/Category.kt
+++ app/src/main/java/com/example/quiz/model/Category.kt
@@ -1,9 +1,20 @@
 package com.example.quiz.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.quiz.data.local.DbScheme.CategoryTable
 import kotlin.random.Random
 
+@Entity(tableName = CategoryTable.TABLE_NAME)
 data class Category(
-    var id: Int = Random.nextInt(),
+    @ColumnInfo(name = CategoryTable.Cols.TEXT)
     var text: String = "",
-    var imageUrl: String = ""
+
+    @ColumnInfo(name = CategoryTable.Cols.IMAGE_URL)
+    var imageUrl: String = "",
+
+    @PrimaryKey
+    @ColumnInfo(name = CategoryTable.Cols.ID)
+    var id: Int = Random.nextInt()
 )
