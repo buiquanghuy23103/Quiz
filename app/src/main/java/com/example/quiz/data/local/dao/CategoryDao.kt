68f098e728e6b000fc06524d2db194a189033b24
huybui <huythdnbkltv@gmail.com>
2019-08-15 17:08:12 +0300

    Add CategoryDao

---
+++ app/src/main/java/com/example/quiz/data/local/dao/CategoryDao.kt
@@ -0,0 +1,15 @@
+package com.example.quiz.data.local.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.quiz.data.local.DbScheme.CategoryTable
+import com.example.quiz.model.Category
+
+interface CategoryDao {
+    @Insert
+    fun saveList(categoryList: List<Category>)
+
+    @Query("SELECT * FROM ${CategoryTable.TABLE_NAME}")
+    fun getAll(): LiveData<List<Category>>
+}
