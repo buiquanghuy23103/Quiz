1c39cc34e136944d4ba5e780db095e11d3b4979b
huybui <huythdnbkltv@gmail.com>
2019-08-15 17:33:56 +0300

    Add Category to SeedDataWorker and fix CategoryDao

--- app/src/main/java/com/example/quiz/data/local/dao/CategoryDao.kt
+++ app/src/main/java/com/example/quiz/data/local/dao/CategoryDao.kt
@@ -1,11 +1,13 @@
 package com.example.quiz.data.local.dao
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.quiz.data.local.DbScheme.CategoryTable
 import com.example.quiz.model.Category
 
+@Dao
 interface CategoryDao {
     @Insert
     fun saveList(categoryList: List<Category>)
