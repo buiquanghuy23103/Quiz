b91acec750a3c509e3c46e58ca93cf7d2f8704bd
huybui <huythdnbkltv@gmail.com>
2019-08-16 19:49:05 +0300

    Update default value of photoUrl

--- app/src/main/java/com/example/quiz/model/Message.kt
+++ app/src/main/java/com/example/quiz/model/Message.kt
@@ -3,5 +3,5 @@ package com.example.quiz.model
 data class Message(
     val userId: Int = 0,
     var text: String = "No text",
-    var photoUrl: String = "No photo URL"
+    var photoUrl: String = "https://static.thenounproject.com/png/340719-200.png"
 )
