e4f30edf0d8f2adcf2a9f92783b5b8ec023ae77b
huybui <huythdnbkltv@gmail.com>
2019-08-17 16:38:26 +0300

    Update message_list_item ui

--- app/src/main/java/com/example/quiz/model/Message.kt
+++ app/src/main/java/com/example/quiz/model/Message.kt
@@ -1,7 +1,7 @@
 package com.example.quiz.model
 
 data class Message(
-    val username: String = "ANONYMOUS",
+    val username: String = "anonymous",
     var text: String = "No text",
     var photoUrl: String = ""
 )
