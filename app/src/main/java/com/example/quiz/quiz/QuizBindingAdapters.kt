98f4abc2337e44ecc3d7addeb08424431c588e1a
huybui <huythdnbkltv@gmail.com>
2019-08-31 18:05:51 +0300

    Refactor bindImageFromUrl()

--- app/src/main/java/com/example/quiz/quiz/QuizBindingAdapters.kt
+++ app/src/main/java/com/example/quiz/quiz/QuizBindingAdapters.kt
@@ -5,17 +5,15 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.bumptech.glide.request.RequestOptions
 
 @BindingAdapter("imageFromUrl")
 fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
     if (!imageUrl.isNullOrEmpty()) {
         Glide.with(view.context)
             .load(imageUrl)
-            .apply(RequestOptions().override(100, 100))
             .transition(DrawableTransitionOptions.withCrossFade())
             .into(view)
-    } else view.visibility = View.GONE
+    }
 }
 
 @BindingAdapter("isVisible")
