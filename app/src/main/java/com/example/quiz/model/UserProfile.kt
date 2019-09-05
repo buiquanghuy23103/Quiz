90accd629db456e81af9322976875a152266ed11
huybui <huythdnbkltv@gmail.com>
2019-09-05 06:30:05 +0300

    Create method to convert FirebaseUser into UserProfile

--- app/src/main/java/com/example/quiz/model/UserProfile.kt
+++ app/src/main/java/com/example/quiz/model/UserProfile.kt
@@ -1,8 +1,19 @@
 package com.example.quiz.model
 
+import com.google.firebase.auth.FirebaseUser
+
 data class UserProfile(
     var uid: String = "a123",
     var name: String = "unknown",
     var email: String = "email@gmail.com",
     var photoUrl: String = ""
-)
+) {
+    companion object {
+        fun from(user: FirebaseUser) = UserProfile(
+            uid = user.uid,
+            name = user.displayName ?: "anonymous",
+            email = user.email ?: "huythdnbkltv@gmail.com",
+            photoUrl = user.photoUrl.toString()
+        )
+    }
+}
