d23b1674ec90f48bc995a6d75624c3718f256148
huybui <huythdnbkltv@gmail.com>
2019-09-06 16:30:35 +0300

    CategoryListFragment: Add toolbar

--- app/src/main/java/com/example/quiz/categorylist/CategoryListFragment.kt
+++ app/src/main/java/com/example/quiz/categorylist/CategoryListFragment.kt
@@ -2,6 +2,7 @@ package com.example.quiz.categorylist
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.example.quiz.R
 import com.example.quiz.databinding.CategoryListFragmentBinding
@@ -16,6 +17,15 @@ class CategoryListFragment : BaseFragment<CategoryListViewModel, CategoryListFra
     override fun getLayoutId() = R.layout.category_list_fragment
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupToolbar()
+        setupCategoryList()
+    }
+
+    private fun setupToolbar() {
+        (activity as AppCompatActivity).setSupportActionBar(category_tool_bar)
+    }
+
+    private fun setupCategoryList() {
         with(category_list) {
             adapter = CategoryListAdapter().apply {
                 viewModel.categoryList.observe(this@CategoryListFragment, Observer {
