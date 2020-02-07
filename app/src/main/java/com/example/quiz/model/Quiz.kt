a3e1e2227428b41ceb0c89072f429ee5589f3ff9
Huy Bui <huythdnbkltv@gmail.com>
2020-02-07 16:22:28 +0200

    Add options to Quiz model

--- app/src/main/java/com/example/quiz/model/Quiz.kt
+++ app/src/main/java/com/example/quiz/model/Quiz.kt
@@ -21,6 +21,10 @@ data class Quiz(
     var categoryId: Int = 0,
     var explanation: String? = null,
     var imageUrl: String = "https://www.vegetables.co.nz/assets/Uploads/vegetables.jpg",
+    var optionA: String = "Canberra is the capital of Australia",
+    var optionB: String = "Canberra is the capital of Australia",
+    var optionC: String = "Canberra is the capital of Australia",
+    var optionD: String = "Canberra is the capital of Australia",
 
     @PrimaryKey
     var id: Int = Random.nextInt()
