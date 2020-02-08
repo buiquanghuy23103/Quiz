074c9d2b46368d2ef4722a0ddc7f7de46ceb4e06
Huy Bui <huythdnbkltv@gmail.com>
2020-02-08 12:59:50 +0200

    Create an empty activity for Splash screen

---
+++ app/src/main/java/com/example/quiz/splashScreen/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.quiz.splashScreen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
