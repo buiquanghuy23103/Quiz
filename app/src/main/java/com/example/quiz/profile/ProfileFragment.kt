c379a8ba7bc4a34a1d3e15a016a551233c572833
Huy Bui <huythdnbkltv@gmail.com>
2020-02-09 10:01:34 +0200

    Display user's profile

--- app/src/main/java/com/example/quiz/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/quiz/profile/ProfileFragment.kt
@@ -1,6 +1,9 @@
 package com.example.quiz.profile
 
 
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
 import com.example.quiz.R
 import com.example.quiz.databinding.FragmentProfileBinding
 import com.example.quiz.framework.BaseFragment
@@ -12,6 +15,11 @@ class ProfileFragment : BaseFragment<ProfileViewModel, FragmentProfileBinding>()
     override fun initViewModel()
             = getViewModel{ ProfileViewModel() }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.userDetails.observe(viewLifecycleOwner, Observer {
+            binding.user = it
+        })
+    }
 
 }
