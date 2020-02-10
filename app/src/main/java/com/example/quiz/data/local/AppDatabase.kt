d0ef94822db60a792a4c25414803de2415366790
Huy Bui <huythdnbkltv@gmail.com>
2020-02-10 16:50:19 +0200

    Remove unused models

--- app/src/main/java/com/example/quiz/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/quiz/data/local/AppDatabase.kt
@@ -3,24 +3,21 @@ package com.example.quiz.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.quiz.data.local.dao.CategoryDao
-import com.example.quiz.data.local.dao.ChoiceDao
 import com.example.quiz.data.local.dao.QuizDao
 import com.example.quiz.data.local.dao.ScoreDao
 import com.example.quiz.model.Category
-import com.example.quiz.model.Choice
 import com.example.quiz.model.Quiz
 import com.example.quiz.model.Score
 
 // The implementation of this class is based on Android Sunflower
 // https://github.com/googlesamples/android-sunflower
 @Database(
-    entities = [Category::class, Quiz::class, Choice::class, Score::class],
+    entities = [Category::class, Quiz::class, Score::class],
     version = 1,
     exportSchema = false
 )
 abstract class AppDatabase: RoomDatabase() {
     abstract val quizDao: QuizDao
-    abstract val choiceDao: ChoiceDao
     abstract val categoryDao: CategoryDao
     abstract val scoreDao: ScoreDao
 }
