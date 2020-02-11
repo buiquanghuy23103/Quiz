ac5e1a5c550fd13556fa20da658705144bee55c4
Huy Bui <huythdnbkltv@gmail.com>
2020-02-11 09:27:47 +0200

    User cannot re-answer old quizzes

--- app/src/main/java/com/example/quiz/data/local/dao/ScoreDao.kt
+++ app/src/main/java/com/example/quiz/data/local/dao/ScoreDao.kt
@@ -1,5 +1,6 @@
 package com.example.quiz.data.local.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import com.example.quiz.model.Score
@@ -10,5 +11,8 @@ interface ScoreDao: BaseDao<Score> {
 
     @Query("SELECT * FROM score WHERE userId = :userId AND categoryId = :categoryId")
     fun find(userId: String, categoryId: String): Single<Score>
+
+    @Query("SELECT quizId FROM score WHERE categoryId = :categoryId")
+    fun getAlreadyAnsweredQuizIds(categoryId: String): LiveData<List<String>>
 
 }
