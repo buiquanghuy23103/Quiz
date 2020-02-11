27754c6d48e26130f57c71862637563ea8e766fc
Huy Bui <huythdnbkltv@gmail.com>
2020-02-11 15:01:03 +0200

    Display score for each category

--- app/src/main/java/com/example/quiz/data/local/dao/CategoryDao.kt
+++ app/src/main/java/com/example/quiz/data/local/dao/CategoryDao.kt
@@ -3,6 +3,8 @@ package com.example.quiz.data.local.dao
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
+import com.example.quiz.model.Achievement
 import com.example.quiz.model.Category
 import io.reactivex.Observable
 
@@ -17,4 +19,9 @@ interface CategoryDao : BaseDao<Category> {
 
     @Query("SELECT * FROM categories WHERE id = :categoryId")
     fun getById(categoryId: String): LiveData<Category>
+
+    @Transaction
+    @Query("SELECT * FROM categories")
+    fun getAchievements(): LiveData<List<Achievement>>
+
 }
---
+++ app/src/main/java/com/example/quiz/profile/AchievementListAdapter.kt
@@ -0,0 +1,72 @@
+package com.example.quiz.profile
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.quiz.R
+import com.example.quiz.databinding.AchievementItemBinding
+import com.example.quiz.model.Achievement
+
+class AchievementListAdapter(private val userId: String?)
+    : ListAdapter<Achievement, AchievementListAdapter.AchievementItem>(diffCallback)
+{
+
+
+    companion object {
+        val diffCallback = object: DiffUtil.ItemCallback<Achievement>() {
+            override fun areItemsTheSame(oldItem: Achievement, newItem: Achievement): Boolean {
+                return oldItem.category.id == newItem.category.id
+            }
+
+            override fun areContentsTheSame(oldItem: Achievement, newItem: Achievement): Boolean {
+                return oldItem.category.text == newItem.category.text &&
+                        oldItem.quizzes.size == newItem.quizzes.size &&
+                        oldItem.scores.size == newItem.scores.size
+
+            }
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AchievementItem {
+        return AchievementItem.from(parent, userId)
+    }
+
+    override fun onBindViewHolder(holder: AchievementItem, position: Int) {
+        val achievement = getItem(position)
+        holder.bind(achievement)
+    }
+
+    class AchievementItem private constructor(
+        private val binding: AchievementItemBinding,
+        private val userId: String?
+    ): RecyclerView.ViewHolder(binding.root) {
+
+        companion object {
+            fun from(parent: ViewGroup, userId: String?): AchievementItem {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding: AchievementItemBinding
+                    = DataBindingUtil.inflate(inflater, R.layout.achievement_item, parent, false)
+                return AchievementItem(binding, userId)
+            }
+        }
+
+        fun bind(achievement: Achievement) {
+            binding.categoryName.text = achievement.category.text
+            val totalQuizzes = achievement.quizzes.size
+
+            if (totalQuizzes > 0 && userId != null) {
+                val currentUserScore = achievement.scores.filter { it.userId == userId }
+                val totalScores = currentUserScore.size
+                binding.numberOfAnsweredQuiz.text = "$totalScores / $totalQuizzes"
+                binding.achievementProgressBar.progress = (totalScores / totalQuizzes) * 100
+            }
+
+        }
+
+    }
+
+}
